f2639b289fc32a20ed2260e538b396e7edf52980
Luuk Veenis <luukveenis@shaw.ca>
2015-03-21 13:28:37 -0700

    Implement reading OS name from FAT disk image
    Added a generic function read_str that can be used to copy arbitrary
    length strings into a buffer from the file system.

--- diskinfo.c
+++ diskinfo.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+/* Copies the string of given length from data into buf,
+ * starting at the offset */
+void read_str(char *buf, char *data, size_t offset, size_t length){
+  strncpy(buf, (data + offset), length);
+  buf[length] = '\0';
+}
+
 int main(int argc, char **argv){
   int fd;
   char *data;
+  char os_name[9]; /* Leave room for terminating '\0' */
   struct stat sf;
 
   if (argc != 2){
@@ -16,6 +25,9 @@ int main(int argc, char **argv){
   if ((fd = open(argv[1], O_RDONLY))){
     fstat(fd, &sf);
     data = mmap(NULL,sf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+
+    read_str(os_name, data, 3, 8);
+    printf("OS Name: %s\n", os_name);
   } else {
     printf("Failed to open file '%s'\n", argv[1]);
   }
