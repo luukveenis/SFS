0062162e02df9a38683012627706696b9bc6c4dc
Luuk Veenis <luukveenis@shaw.ca>
2015-03-21 14:18:06 -0700

    Read sector size and total size of disk
    Added a generic function to read arbitrary length numbers from the disk
    image and convert them from little endian to big endian.

--- diskinfo.c
+++ diskinfo.c
@@ -5,6 +5,26 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+int read_num(char *data, size_t offset, size_t size){
+  int retval, i;
+
+  for(i=0, retval=0; i<size; i++){
+    retval += data[offset+i] << (8*i);
+  }
+  return retval;
+}
+
+int get_total_size(char *data, int secsize){
+  int totalsecs;
+
+  totalsecs = read_num(data, 19, 2);
+  if (totalsecs == 0){
+    totalsecs = read_num(data, 32, 4);
+  }
+
+  return (totalsecs * secsize);
+}
+
 /* Copies the string of given length from data into buf,
  * starting at the offset */
 void read_str(char *buf, char *data, size_t offset, size_t length){
@@ -13,7 +33,7 @@ void read_str(char *buf, char *data, size_t offset, size_t length){
 }
 
 int main(int argc, char **argv){
-  int fd;
+  int fd, secsize, totsize;
   char *data;
   char os_name[9]; /* Leave room for terminating '\0' */
   struct stat sf;
@@ -26,8 +46,13 @@ int main(int argc, char **argv){
     fstat(fd, &sf);
     data = mmap(NULL,sf.st_size, PROT_READ, MAP_SHARED, fd, 0);
 
+    secsize = read_num(data, 11, 2);
+    totsize = get_total_size(data, secsize);
     read_str(os_name, data, 3, 8);
+
     printf("OS Name: %s\n", os_name);
+    printf("Sector size: %d\n", secsize);
+    printf("Total size: %d\n", totsize);
   } else {
     printf("Failed to open file '%s'\n", argv[1]);
   }
