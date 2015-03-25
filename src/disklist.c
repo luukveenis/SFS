4a8a6bd95543871ecb4164cc6208c7dea8de503c
Luuk Veenis <luukveenis@shaw.ca>
2015-03-25 13:54:59 -0700

    Begin implementing root directory listing
    - add function that loops through root directory entries
    - add data structures to store date and time information
    - add date parsing function

--- src/disklist.c
+++ src/disklist.c
@@ -4,8 +4,49 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+#include "util.h"
+
+struct date {
+  int day;
+  int month;
+  int year;
+};
+
+struct time {
+  int hours;
+  int mins;
+};
+
+void parse_date(struct date *fdate, char *data, int offset){
+  int date = (data[offset+17] << 8) + data[offset+16];
+  fdate->year = 1980 + ((date & 0xFE00) >> 9);
+  fdate->month = (date & 0x01E0) >> 5;
+  fdate->day = (date & 0x001F);
+}
+
+void list_root(char *data, int secsize){
+  char fname[9];
+  struct date fdate;
+  /* struct time ftime; */
+  int i, is_file, fsize;
+
+  for (i=(19*secsize); i<(33*secsize); i+=32){
+    if ((int)data[i] == 0xE5
+        || (int)data[i+11] == 0x0F
+        || data[i+11] & 0x08) continue;
+    if (!data[i]) return; /* No more files */
+
+    is_file = (data[i+11] & 0x10) ? 0 : 1;
+    fsize = read_num(data, (i+28), 4);
+    read_str(fname, data, i, 8);
+    parse_date(&fdate, data, i);
+    printf("Filename: %s\n", fname);
+    printf("Year: %d, Month: %d, Day: %d\n", fdate.year, fdate.month, fdate.day);
+  }
+}
+
 int main(int argc, char **argv){
-  int fd;
+  int fd, secsize;
   char *data;
   struct stat sf;
 
@@ -18,6 +59,8 @@ int main(int argc, char **argv){
     data = mmap(NULL,sf.st_size, PROT_READ, MAP_SHARED, fd, 0);
 
     /* Implement root directory listing here */
+    secsize = read_num(data, 11, 2);
+    list_root(data, secsize);
   } else {
     printf("Failed to open file '%s'\n", argv[1]);
   }
