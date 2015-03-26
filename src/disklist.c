cd2c8c9640c06e3c7aacc4924fe0a996ce164cd1
Luuk Veenis <luukveenis@shaw.ca>
2015-03-25 20:23:30 -0700

    Move file data into its own struct

--- src/disklist.c
+++ src/disklist.c
@@ -18,6 +18,14 @@ struct time {
   int secs;
 };
 
+struct fdata {
+  int is_file;
+  char name[9];
+  int size;
+  struct date cdate;
+  struct time ctime;
+};
+
 void parse_date(struct date *fdate, unsigned char *data, int offset){
   int date = (data[offset+17] << 8) + data[offset+16];
   fdate->year = 1980 + ((date & 0xFE00) >> 9);
@@ -33,10 +41,8 @@ void parse_time(struct time *ftime, unsigned char *data, int offset){
 }
 
 void list_root(unsigned char *data, int secsize){
-  char fname[9];
-  struct date fdate;
-  struct time ftime;
-  int i, is_file, fsize;
+  struct fdata file;
+  int i;
 
   for (i=(19*secsize); i<(33*secsize); i+=32){
     if ((int)data[i] == 0xE5
@@ -44,14 +50,11 @@ void list_root(unsigned char *data, int secsize){
         || data[i+11] & 0x08) continue;
     if (!data[i]) return; /* No more files */
 
-    is_file = (data[i+11] & 0x10) ? 0 : 1;
-    fsize = read_num(data, (i+28), 4);
-    read_str(fname, data, i, 8);
-    parse_date(&fdate, data, i);
-    parse_time(&ftime, data, i);
-    printf("Filename: %s\n", fname);
-    printf("Year: %d, Month: %d, Day: %d\n", fdate.year, fdate.month, fdate.day);
-    printf("Time: %02d:%02d:%02d\n", ftime.hours, ftime.mins, ftime.secs);
+    file.is_file = (data[i+11] & 0x10) ? 0 : 1;
+    file.size = read_num(data, (i+28), 4);
+    read_str(file.name, data, i, 8);
+    parse_date(&file.cdate, data, i);
+    parse_time(&file.ctime, data, i);
   }
 }
 
